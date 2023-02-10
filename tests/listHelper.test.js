7929c67234178e25599b72fd3bf94bac2640c433
samppa13 <samuelkosola@gmail.com>
2023-02-10 19:39:45 +0200

    4.5: apufunktioita ja yksikkötestejä, step3

--- tests/listHelper.test.js
+++ tests/listHelper.test.js
@@ -1,61 +1,59 @@
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-    const blogs = []
+const listWithZeroBlog = []
 
-    const result = listHelper.dummy(blogs)
+const listWithOneBlog = [
+    {
+        _id: '7d644dd93e76d898456a39b1',
+        title: 'Hockey goalie',
+        author: 'Frans Tuohimaa',
+        url: 'http://www.blogspot.com/hockeyGoalie',
+        likes: 9,
+        __v: 0
+    }
+]
+
+const blogs = [
+    {
+        _id: '7d644dd93e76d898456a39b1',
+        title: 'Hockey goalie',
+        author: 'Frans Tuohimaa',
+        url: 'http://www.blogspot.com/hockeyGoalie',
+        likes: 9,
+        __v: 0
+    },
+    {
+        _id: '8c544fd42e766493426f32c6',
+        title: 'Hockey goalie goals',
+        author: 'Pekka Rinne',
+        url: 'http://www.wordpress.com/hockey/goalie/goals',
+        likes: 15,
+        __v: 0
+    },
+    {
+        _id: '75643d243e76a823446f3921',
+        title: 'Goalie',
+        author: 'Juuse Saros',
+        url: 'http://www.blogspot.com/goalie/pro',
+        likes: 10,
+        __v: 0
+    },
+    {
+        _id: '556a4ed23576d8a8446439cd',
+        title: 'Goalie history',
+        author: 'Jarmo Myllys',
+        url: 'http://www.history.com/blogs/goalie',
+        likes: 4,
+        __v: 0
+    }
+]
+
+test('dummy returns one', () => {
+    const result = listHelper.dummy(listWithZeroBlog)
     expect(result).toBe(1)
 })
 
 describe('total likes', () => {
-    const listWithZeroBlog = []
-
-    const listWithOneBlog = [
-        {
-            _id: '7d644dd93e76d898456a39b1',
-            title: 'Hockey goalie',
-            author: 'Frans Tuohimaa',
-            url: 'http://www.blogspot.com/hockeyGoalie',
-            likes: 9,
-            __v: 0
-        }
-    ]
-
-    const blogs = [
-        {
-            _id: '7d644dd93e76d898456a39b1',
-            title: 'Hockey goalie',
-            author: 'Frans Tuohimaa',
-            url: 'http://www.blogspot.com/hockeyGoalie',
-            likes: 9,
-            __v: 0
-        },
-        {
-            _id: '8c544fd42e766493426f32c6',
-            title: 'Hockey goalie goals',
-            author: 'Pekka Rinne',
-            url: 'http://www.wordpress.com/hockey/goalie/goals',
-            likes: 15,
-            __v: 0
-        },
-        {
-            _id: '75643d243e76a823446f3921',
-            title: 'Goalie',
-            author: 'Juuse Saros',
-            url: 'http://www.blogspot.com/goalie/pro',
-            likes: 10,
-            __v: 0
-        },
-        {
-            _id: '556a4ed23576d8a8446439cd',
-            title: 'Goalie history',
-            author: 'Jarmo Myllys',
-            url: 'http://www.history.com/blogs/goalie',
-            likes: 4,
-            __v: 0
-        }
-    ]
-
     test('of empty list is zero', () => {
         const result = listHelper.totalLikes(listWithZeroBlog)
         expect(result).toBe(0)
@@ -73,54 +71,6 @@ describe('total likes', () => {
 })
 
 describe('favorite blog', () => {
-    const listWithZeroBlog = []
-
-    const listWithOneBlog = [
-        {
-            _id: '7d644dd93e76d898456a39b1',
-            title: 'Hockey goalie',
-            author: 'Frans Tuohimaa',
-            url: 'http://www.blogspot.com/hockeyGoalie',
-            likes: 9,
-            __v: 0
-        }
-    ]
-
-    const blogs = [
-        {
-            _id: '7d644dd93e76d898456a39b1',
-            title: 'Hockey goalie',
-            author: 'Frans Tuohimaa',
-            url: 'http://www.blogspot.com/hockeyGoalie',
-            likes: 9,
-            __v: 0
-        },
-        {
-            _id: '8c544fd42e766493426f32c6',
-            title: 'Hockey goalie goals',
-            author: 'Pekka Rinne',
-            url: 'http://www.wordpress.com/hockey/goalie/goals',
-            likes: 15,
-            __v: 0
-        },
-        {
-            _id: '75643d243e76a823446f3921',
-            title: 'Goalie',
-            author: 'Juuse Saros',
-            url: 'http://www.blogspot.com/goalie/pro',
-            likes: 10,
-            __v: 0
-        },
-        {
-            _id: '556a4ed23576d8a8446439cd',
-            title: 'Goalie history',
-            author: 'Jarmo Myllys',
-            url: 'http://www.history.com/blogs/goalie',
-            likes: 4,
-            __v: 0
-        }
-    ]
-
     test('of empty list is empty object', () => {
         const result = listHelper.favoriteBlog(listWithZeroBlog)
         expect(result).toEqual({})
