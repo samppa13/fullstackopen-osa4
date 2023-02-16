e45893d50e9a53d05408860e5d5197ac765da3ef
samppa13 <samuelkosola@gmail.com>
2023-02-16 13:55:41 +0200

    4.13: blogilistan laajennus, step1

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -104,6 +104,29 @@ describe('addition of a new blog', () => {
     })
 })
 
+describe('deletion of a blog', () => {
+    test('succeeds with status code 204 if id is valid', async () => {
+        const blogAtStart = await helper.blogsInDb()
+        const blogToDelete = blogAtStart[0]
+        await api
+            .delete(`/api/blogs/${blogToDelete.id}`)
+            .expect(204)
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(
+            helper.initialBlogs.length - 1
+        )
+        const titles = blogsAtEnd.map(b => b.title)
+        expect(titles).not.toContain(blogToDelete.title)
+    })
+
+    test('fails with statuscode 400 id is invalid', async () => {
+        const invalidId = '639431037'
+        await api
+            .delete(`/api/blogs/${invalidId}`)
+            .expect(400)
+    })
+})
+
 afterAll(async () => {
     await mongoose.connection.close()
 })
